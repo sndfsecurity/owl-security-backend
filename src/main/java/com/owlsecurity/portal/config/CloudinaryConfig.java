081cb9ca28494cac185f7e7bbf40b1d51db3cab2
sndfsecurity <sndf4315@gmail.com>
2026-06-24 13:18:12 +0530

    update

--- src/main/java/com/owlsecurity/portal/config/CloudinaryConfig.java
+++ src/main/java/com/owlsecurity/portal/config/CloudinaryConfig.java
@@ -10,13 +10,13 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class CloudinaryConfig {
 
-	@Value("${CLOUDINARY_CLOUD_NAME}")
+	@Value("${cloudinary.cloud-name}")
 	private String cloudName;
 
-	@Value("${CLOUDINARY_API_KEY}")
+	@Value("${cloudinary.api-key}")
 	private String apiKey;
 
-	@Value("${CLOUDINARY_API_SECRET}")
+	@Value("${cloudinary.api-secret}")
 	private String apiSecret;
 	
     @Bean
