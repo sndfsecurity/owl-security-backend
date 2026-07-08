9844e74ca01bb669cae20f49bd2d042a1bb1dc14
sndfsecurity <sndf4315@gmail.com>
2026-07-08 15:56:40 +0530

    Remove unnecessary blank line in CloudinaryConfig

--- src/main/java/com/owlsecurity/portal/config/CloudinaryConfig.java
+++ src/main/java/com/owlsecurity/portal/config/CloudinaryConfig.java
@@ -3,7 +3,6 @@ package com.owlsecurity.portal.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
